2bf589b97f6eb38e548a7ed94db4b6f379d70885
noah.stewart9813@gmail.com <noah.stewart9813@gmail.com>
2018-08-15 19:32:14 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,8 @@
 def using_until
-        levitation_force = 6
-        until levitation_force == 10
-          levitation_force += 1
-          puts levitation_force
-        end
+  levitation_force = 6
+  until levitation_force == 10
+    levitation_force += 1
+    puts levitation_force
+  end
 end
 
